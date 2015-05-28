3606752ab7072054b44463e065f5b0f7deaeb688
Krzyrok <krzyrok@gmail.com>
2015-05-28 04:21:39 +0200

    added bower configs and angular package

--- FsxWeb/FsxWeb/App_Start/BundleConfig.cs
+++ FsxWeb/FsxWeb/App_Start/BundleConfig.cs
@@ -7,8 +7,8 @@
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            //bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-            //            "~/Scripts/jquery-{version}.js"));
+            bundles.Add(new ScriptBundle("~/Content/js").Include(
+                        "~/Content/libs/"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/css/site.css"));
