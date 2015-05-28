d3e425f6d98df931d1112ad1723c2339cbc2558f
Krzyrok <krzyrok@gmail.com>
2015-05-28 03:15:56 +0200

    added implementation of Logger in FsxWeb

---
+++ FsxWeb/FsxWeb/Logging/Logger.cs
@@ -0,0 +1,13 @@
+﻿namespace FsxWeb.Logging
+{
+    using System;
+    using FsxCommunicator.Interfaces;
+
+    public class Logger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
