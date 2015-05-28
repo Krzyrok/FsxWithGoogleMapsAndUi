bc06a893074085be09ebc787351299b8a2f0475a
Krzyrok <krzyrok@gmail.com>
2015-05-28 04:27:13 +0200

    included angular in the bundle

--- FsxWeb/FsxWeb/App_Start/BundleConfig.cs
+++ FsxWeb/FsxWeb/App_Start/BundleConfig.cs
@@ -7,8 +7,9 @@
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/Content/js").Include(
-                        "~/Content/libs/"));
+            var bundle = new ScriptBundle("~/Content/js")
+                .Include("~/Content/libs/angular/angular.js");
+            bundles.Add(bundle);
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/css/site.css"));
