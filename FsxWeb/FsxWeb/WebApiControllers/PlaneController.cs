aa16ea3272a375524b90cd8327bd310cac8dca8f
Krzyrok <krzyrok@gmail.com>
2015-06-10 21:49:22 +0200

    changed configuration; added method for getting data from another api

--- FsxWeb/FsxWeb/WebApiControllers/PlaneController.cs
+++ FsxWeb/FsxWeb/WebApiControllers/PlaneController.cs
@@ -1,7 +1,12 @@
 ﻿namespace FsxWeb.WebApiControllers
 {
+    using System;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Threading.Tasks;
     using System.Web.Http;
     using FsxCommunicator;
+    using FsxCommunicator.Model;
     using Logging;
 
     public class PlaneController : ApiController
@@ -14,15 +19,44 @@
         }
 
         // GET: api/Plane
-        public IHttpActionResult Get()
+        public async Task<IHttpActionResult> Get()
         {
-            var planeData = _fsx.GetCurrentPlaneData();
+            //var planeData = new PlaneData {Location = new Location {Latitude = 50.0, Longitude = 50.0}};
+            PlaneData planeData = null;
+            try
+            {
+                //planeData = _fsx.GetCurrentPlaneData();
+                planeData = await GetFromWebApi();
+            }
+            catch (Exception)
+            {
+                // some error
+            }
 
             if (planeData == null)
             {
-                return NotFound();
+                return BadRequest("cannot connect to the FSX");
             }
             return Ok(planeData);
+        }
+
+        private async Task<PlaneData> GetFromWebApi()
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:8045/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await client.GetAsync("api/plane");
+                if (response.IsSuccessStatusCode)
+                {
+                    PlaneData product = await response.Content.ReadAsAsync<PlaneData>();
+                    return product;
+                }
+
+                return null;
+            }
         }
     }
 }
